d69add3ffbebb23030dc2d284f2b3378c0770294
sean <sean.snow@live.com>
2025-12-28 14:48:34 +0800

    fix: contains

--- lib/core/string.dart
+++ lib/core/string.dart
@@ -8,6 +8,14 @@ extension StringExtension on String? {
   }
 
   bool get isNotEmpty {
-    return this != null && this.isNotEmpty;
+    return this != null && (this ?? '').isNotEmpty;
+  }
+
+  int get length {
+    return this == null ? 0 : this.length;
+  }
+
+  bool contains(Pattern other, [int startIndex = 0]) {
+    return this == null ? false : this.contains(other, startIndex);
   }
 }
