74ebaea478a1d9e4279ad54ed4a5a09b42a29111
sean <sean.snow@live.com>
2025-12-28 14:37:38 +0800

    fix: ext string

--- lib/core/string.dart
+++ lib/core/string.dart
@@ -1,5 +1,13 @@
 extension StringExtension on String? {
+  bool get isEmpty {
+    return this == null || this.isEmpty;
+  }
+
   bool get isEmptyOrNull {
     return this == null || this!.isEmpty;
+  }
+
+  bool get isNotEmpty {
+    return this != null && this.isNotEmpty;
   }
 }
