1155003a61be168e28a0e0cac34d733790da6761
sean <sean.snow@live.com>
2026-01-02 21:20:35 +0800

    fix: 修复死循环

--- lib/core/string.dart
+++ lib/core/string.dart
@@ -1,6 +1,6 @@
 extension StringExtension on String? {
   bool get isEmpty {
-    return this == null || this.isEmpty;
+    return this == null || this!.isEmpty;
   }
 
   bool get isEmptyOrNull {
@@ -12,10 +12,10 @@ extension StringExtension on String? {
   }
 
   int get length {
-    return this == null ? 0 : this.length;
+    return this == null ? 0 : this!.length;
   }
 
   bool contains(Pattern other, [int startIndex = 0]) {
-    return this == null ? false : this.contains(other, startIndex);
+    return this == null ? false : this!.contains(other, startIndex);
   }
 }
