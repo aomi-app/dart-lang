49e52e501783de7813e36de3e94f1fd6f520d305
sean <sean.snow@live.com>
2026-01-04 22:48:22 +0800

    feat: to color

--- lib/core/string.dart
+++ lib/core/string.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 extension StringExtension on String? {
   bool get isEmpty {
     return this == null || this!.isEmpty;
@@ -17,5 +19,18 @@ extension StringExtension on String? {
 
   bool contains(Pattern other, [int startIndex = 0]) {
     return this == null ? false : this!.contains(other, startIndex);
+  }
+}
+
+extension ColorStringExtensions on String {
+  Color? toColor() {
+    var hexColor = replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF$hexColor";
+    }
+    if (hexColor.length == 8) {
+      return Color(int.parse("0x$hexColor"));
+    }
+    return null;
   }
 }
